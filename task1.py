d38d37a8250f5a2a506bd5a702f0f964a3824658
Yuri <yuriiol@proton.me>
2023-04-29 21:05:07 +0300

    deleted print

--- task1.py
+++ task1.py
@@ -3,5 +3,5 @@ from read_write import write
 
 def task1(df):
     result_df = df.filter(df.language == "uk").select(df.title)
-    print(result_df.count())
+    # print(result_df.count())
     write(result_df, "results/task01")
